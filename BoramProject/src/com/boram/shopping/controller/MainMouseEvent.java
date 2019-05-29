2e3f07247169241e9b2ec85d5c886c6ccd958e35
fish <fish@DESKTOP-T4H9JF7>
2019-05-29 12:51:00 +0900

    신훈 - 마이페이지 다른곳으로 변경

--- BoramProject/src/com/boram/shopping/controller/MainMouseEvent.java
+++ BoramProject/src/com/boram/shopping/controller/MainMouseEvent.java
@@ -1,20 +1,14 @@
 package com.boram.shopping.controller;
 
-import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
-import com.boram.shopping.view.MainView;
 
 public class MainMouseEvent extends MouseAdapter{
 	JPanel subMenu;
 	JScrollPane subMenuScroll;
-	JPanel mainPage;
-	JFrame frame;
 	String what;
 
 	public MainMouseEvent(JPanel subMenu, JScrollPane subMenuScroll,String what) {
@@ -23,19 +17,6 @@ public class MainMouseEvent extends MouseAdapter{
 		this.what = what;
 	}
 	
-/*	public MainMouseEvent(JFrame frame, JPanel mainPage) {
-		
-		mainPage.removeAll();
-
-//		MainView.mainPageScroll.setViewportView(null);
-		this.mainPage = mainPage;
-		mainPage.setPreferredSize(new Dimension(450, 1000));
-		MainView.mainPageScroll.setViewportView(mainPage);
-
-		frame.revalidate();
-		frame.repaint();
-	}
-	*/
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if(what.equals("서브메뉴닫기")) {
@@ -44,6 +25,5 @@ public class MainMouseEvent extends MouseAdapter{
 		else if(what.equals("서브메뉴열기")) {
 			new MainThreadEvent(subMenu,subMenuScroll,-376,0,"서브메뉴열기").start();
 		}
-		
 	}
 }
